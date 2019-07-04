130c5ee5fa961d48a61f4454979934ba75ab018b
Nur Muhammad Luthfi <33769324+nmluthfi@users.noreply.github.com>
2019-07-04 14:24:07 +0700

    add project 3

---
+++ KotlinGame/src/Game.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+    val game = Game()
+    while (true) {
+        print("Enter a direction: n/s/e/w: ")
+        game.makeMove(readLine())
+    }
+}
+
+class Game {
+
+    var path = mutableListOf(Direction.START)
+    val north = { path.add(Direction.NORTH)}
+    val south = { path.add(Direction.SOUTH)}
+    val east = { path.add(Direction.EAST)}
+
+    val west = { path.add(Direction.WEST)}
+
+    val end = {
+        path.add(Direction.END)
+        println("Game Over: $path")
+        path.clear()
+        false
+    }
+
+    fun makeMove(command: String?) {
+        if (command.equals("n")) move(north)
+        else if (command.equals("s")) move(south)
+        else if (command.equals("e")) move(east)
+        else if (command.equals("w")) move(west)
+        else move(end)
+    }
+}
+
+fun move(where: () -> Boolean) {
+    where.invoke()
+}
